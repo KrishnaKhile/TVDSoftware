015f8414dd56a92be21a26e5d27bb7cabdcf556b
KrishnaKhile <103939308+KrishnaKhile@users.noreply.github.com>
2022-11-18 21:38:33 +0530

    add css file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/aos/aos.css";
+import "./assets/glightbox/css/glightbox.min.css";
+import "./assets/swiper/swiper-bundle.min.css";
+import "./assets/css/style.css";
+
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
